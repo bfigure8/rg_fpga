0e1e8ceedfb90e50c54b841856bc3c2bfdf8081a
bfigure8 <bfigure8@gmail.com>
2014-03-10 22:22:24 +0800

    Version 1.0 added dac_interface_rtl.vhd

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 bfigure8
 09/03/2014 Base Version of rg_fpga
+10/03/2014 Version 1.0 of rg_fpga
---
+++ rg_fpga/fmc230/designs/fpga_fmc230/hdl/dac_interface_rtl.vhd
@@ -0,0 +1,45 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : dac_interface_rtl.vhd
+-- Author     : steel_lake
+-- Company    : Rohde & Schwarz
+-- Last update: 
+-- Platform   : PC
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date     Version     Author      Description
+-------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity dac_interface is
+    generic (
+        DAC_WIDTH : positive := 14
+    )        
+    port (
+        i_clk       : in std_logic;
+        i_reset     : in std_logic;
+        i_data_in   : in std_logic_vector(DAC_WIDTH-1 downto 0);
+        i_data_out  : out std_logic_vector(DAC_WIDTH-1 downto 0)
+    );                
+end entity dac_interface;
+--
+architecture rtl of dac_interface is
+begin
+    
+    p_dac : process(i_clk, i_reset)
+    begin
+        if i_reset = '1' then
+            o_data_out <= (others => '0');
+        elsif rising_edge(i_clk) then
+            o_data_out <= i_data_in;
+        end if;
+    end process;            
+            
+end architecture rtl;
+
